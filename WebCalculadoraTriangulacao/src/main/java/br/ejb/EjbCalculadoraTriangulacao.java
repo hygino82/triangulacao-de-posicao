ecfedbd85d3981bad06add910953d639cfe4b8ec
Adroaldo <hygino@msn.com>
2024-10-19 17:13:49 +0000

    Determinação do ponto OK

--- WebCalculadoraTriangulacao/src/main/java/br/ejb/EjbCalculadoraTriangulacao.java
+++ WebCalculadoraTriangulacao/src/main/java/br/ejb/EjbCalculadoraTriangulacao.java
@@ -11,11 +11,13 @@ public class EjbCalculadoraTriangulacao implements ICalculadoraTriangulacao {
     public Ponto triangular(Ponto a, Ponto b, Ponto c, double da, double db, double dc) {
         double m[][] = {
             {2 * (-a.getX() + b.getX()), 2 * (-a.getY() + b.getY()),
-                quad(da) - quad(db) - quad(a.getX()) + quad(b.getY()) - quad(a.getY()) + quad(b.getY())},
+                quad(da) - quad(db) - quad(a.getX()) + quad(b.getX()) - quad(a.getY()) + quad(b.getY())},
             {2 * (-a.getX() + c.getX()), 2 * (-a.getY() + c.getY()),
                 quad(da) - quad(dc) - quad(a.getX()) + quad(c.getX()) - quad(a.getY()) + quad(c.getY())}
+
         };
         double pivo = m[1][0] / m[0][0];
+
         for (int j = 0; j < 3; j++) {
             m[1][j] -= pivo * m[0][j];
         }
--- WebCalculadoraTriangulacao/src/main/java/br/jsf/JsfCalculadoraTriangulacao.java
+++ WebCalculadoraTriangulacao/src/main/java/br/jsf/JsfCalculadoraTriangulacao.java
@@ -1,9 +1,11 @@
 package br.jsf;
 
+import br.model.Ponto;
 import bri.ICalculadoraTriangulacao;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import lombok.*;
 
 @Named(value = "jsfCalculadoraTriangulacao")
 @RequestScoped
@@ -12,8 +14,53 @@ public class JsfCalculadoraTriangulacao {
     @EJB
     private ICalculadoraTriangulacao ejbCalculadoraTriangulacao;
 
-
     public JsfCalculadoraTriangulacao() {
     }
-    
+
+    @Getter
+    @Setter
+    private double xa = -6;
+
+    @Getter
+    @Setter
+    private double ya = 2;
+
+    @Getter
+    @Setter
+    private double da = 5;
+
+    @Getter
+    @Setter
+    private double xb = 3;
+
+    @Getter
+    @Setter
+    private double yb = 2;
+
+    @Getter
+    @Setter
+    private double db = 4;
+
+    @Getter
+    @Setter
+    private double xc = -1;
+
+    @Getter
+    @Setter
+    private double yc = -4;
+
+    @Getter
+    @Setter
+    private double dc = 6;
+
+    @Getter
+    private Ponto ponto;
+
+    public void calcularPonto() {
+        Ponto a = new Ponto(xa, ya);
+        Ponto b = new Ponto(xb, yb);
+        Ponto c = new Ponto(xc, yc);
+
+        ponto = ejbCalculadoraTriangulacao.triangular(a, b, c, da, db, dc);
+    }
 }
